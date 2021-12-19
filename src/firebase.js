1de8ce94fa0e22e53bcd2c0d0898011a842c2269
hemantsarthak <hemantsarthak1@gmail.com>
2021-12-19 22:19:36 +0530

    Initial

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,23 @@ import "firebase/storage";
 import "firebase/firestore";
 
 // initialize firebaseApp with firebase-config values
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+};*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDkQtwFfRDlRvblPpBrUdVWDq7QRPifi5c",
+  authDomain: "linkedin-9b4dd.firebaseapp.com",
+  projectId: "linkedin-9b4dd",
+  storageBucket: "linkedin-9b4dd.appspot.com",
+  messagingSenderId: "49701505785",
+  appId: "1:49701505785:web:d82a890d06bb2f396d3028"
 };
+
 
 let app;
 
